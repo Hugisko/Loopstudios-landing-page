213359e0452f55367f4e184e1bd0a104174b36c9
Hung Do Quoc <hung.do.quoc@student.tuke.sk>
2023-05-27 22:09:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector(".hamburger");
+const close_btn = document.querySelector(".close");
+const nav = document.querySelector(".nav");
+const cards = document.querySelectorAll(".card");
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+close_btn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+          const card = entry.target;
+          card.style.opacity = "1";
+          observer.unobserve(entry.target);
+        }
+    });
+  }, options);
+
+cards.forEach(card => {
+    observer.observe(card);
+});
